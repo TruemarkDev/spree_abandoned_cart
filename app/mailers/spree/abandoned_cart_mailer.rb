70381a7d8209f9b306518791c795a89f3f733bd2
anatolii-volodko <anatolii@jetruby.com>
2015-08-27 19:52:48 +0300

    Campaign for three emails sending

--- app/mailers/spree/abandoned_cart_mailer.rb
+++ app/mailers/spree/abandoned_cart_mailer.rb
@@ -2,10 +2,19 @@ module Spree
   class AbandonedCartMailer < ActionMailer::Base
     default from: Spree::AbandonedCartEmailConfig::Config.email_from
 
-     def abandoned_email(order)
-       @order = order
-       mail to: @order.email, subject: Spree.t("abandoned_cart.email.subject")
-     end
+    def abandoned_email(order)
+      @order = order
+      mail to: @order.email, subject: Spree.t('abandoned_cart.email.subject')
+    end
 
+    def abandoned_second_email(order)
+      @order = order
+      mail to: @order.email, subject: Spree.t('abandoned_cart.email.subject_second'),
+    end
+
+    def abandoned_third_email(order)
+      @order = order
+      mail to: @order.email, subject: Spree.t('abandoned_cart.email.subject_third'),
+    end
   end
 end
--- app/models/spree/abandoned_order.rb
+++ app/models/spree/abandoned_order.rb
@@ -1,7 +1,34 @@
 class Spree::AbandonedOrder < ActiveRecord::Base
+  COUNTER_THIRD_ATTEMPT = 2
+
   belongs_to :order, class_name: 'Spree::Order', foreign_key: :spree_order_id
 
   after_create :increment_send_emails_count
+
+  def self.email_second_eligible_abandoned_email_orders
+    eligible_orders_next_attempt.each {|abandoned_order| abandoned_order.send_second_abandoned_email }
+  end
+
+  def self.email_third_eligible_abandoned_email_orders
+    eligible_orders_next_attempt(COUNTER_THIRD_ATTEMPT).each {|abandoned_order| abandoned_order.send_third_abandoned_email }
+  end
+
+  def self.eligible_orders_next_attempt(emails_count=1, hours_period=[10, 0])
+    # def self.eligible_orders_next_attempt(emails_count=1, hours_period=[25, 24])
+    where(send_emails_count: emails_count,
+          order_accomplished: false,
+          updated_at: (Time.zone.now - hours_period[0].hours)..(Time.zone.now - hours_period[1].hours))
+  end
+
+  def send_second_abandoned_email
+    Spree::AbandonedCartMailer.abandoned_second_email(self.order).deliver
+    increment_send_emails_count
+  end
+
+  def send_third_abandoned_email
+    Spree::AbandonedCartMailer.abandoned_third_email(self.order).deliver
+    increment_send_emails_count
+  end
 
   private
 
