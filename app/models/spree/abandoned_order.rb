0d3f19d4ad5be414a1918ffe3738a3cb95726ec6
Dmitriy Baklikov <dmitriy@railsmuffin.com>
2015-09-11 15:24:36 +0300

    Destroy orders without emails

--- app/models/spree/abandoned_order.rb
+++ app/models/spree/abandoned_order.rb
@@ -6,11 +6,11 @@ class Spree::AbandonedOrder < ActiveRecord::Base
   after_create :increment_send_emails_count
 
   def self.email_second_eligible_abandoned_email_orders
-    eligible_orders_next_attempt.each { |abandoned_order| abandoned_order.send_second_abandoned_email }
+    eligible_orders_next_attempt.each { |abandoned_order| abandoned_order.process_second_abandoned_email }
   end
 
   def self.email_third_eligible_abandoned_email_orders
-    eligible_orders_next_attempt(COUNTER_THIRD_ATTEMPT).each { |abandoned_order| abandoned_order.send_third_abandoned_email }
+    eligible_orders_next_attempt(COUNTER_THIRD_ATTEMPT).each { |abandoned_order| abandoned_order.process_third_abandoned_email }
   end
 
   def self.eligible_orders_next_attempt(emails_count = 1, hours_period = [25, 23])
@@ -21,12 +21,16 @@ class Spree::AbandonedOrder < ActiveRecord::Base
           updated_at: (Time.zone.now - hours_period[0].hours)..(Time.zone.now - hours_period[1].hours))
   end
 
-  def send_second_abandoned_email
+  def process_second_abandoned_email
+    (delete && return) unless self.order.email.present?
+
     Spree::AbandonedCartMailer.abandoned_second_email(self.order).deliver
     increment_send_emails_count
   end
 
-  def send_third_abandoned_email
+  def process_third_abandoned_email
+    (delete && return) unless self.order.email.present?
+
     Spree::AbandonedCartMailer.abandoned_third_email(self.order).deliver
     increment_send_emails_count
   end
